b343e4f7991d0a7c9a7428ff8792b267ef12e332
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-10 14:23:53 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,4 +2,14 @@
 class Person 
   def name
     @name 
+  end 
+  def name=(new_name)
+    @name=new_name
+  end 
+  def job 
+    @job 
+    def job=(new_job)
+      @job=new_job
+    end 
+  end 
     
