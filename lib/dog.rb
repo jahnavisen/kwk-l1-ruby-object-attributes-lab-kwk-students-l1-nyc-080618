52652eb57627a4f7488ef5ecc4fcb31e14e5907f
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-10 14:18:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,12 +36,13 @@ class Dog
   def breed 
     @breed 
   end 
-  def name=(name)
-    @name=name 
-  end 
-  def breed=(breed)
-    @breed=breed 
-  end 
 end 
+#   def name=(name)
+#     @name=name 
+#   end 
+#   def breed=(breed)
+#     @breed=breed 
+#   end 
+# end 
 
 
