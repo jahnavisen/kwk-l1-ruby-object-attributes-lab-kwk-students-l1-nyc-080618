3426a8b953703e66ffc59e40eaabe6b068f76281
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-10 14:16:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,41 @@
-# dog.rb
+# # # dog.rb
+# class Dog 
+#   def initialize (name, breed)
+#     @name = name 
+#     @breed = breed 
+#   end
+#   def name= (new_name)
+#     @name = new_name
+#   end 
+#   def breed= (new_breed)
+#     @breed = new_breed
+#   end 
+# end 
+
+
+# class Dog 
+#   def initialize (name) 
+#     @name = name 
+#   end 
+#   def name 
+#     return @name 
+#   end 
+# end 
+# dog = Dog.new 
+# puts dog.name 
+
+
+class Dog 
+  def initialize (name, breed)
+    @name = name 
+    @breed = breed  
+  end 
+  def name=(name)
+    @name=name 
+  end 
+  def breed=(breed)
+    @breed=breed 
+  end 
+end 
+
+
